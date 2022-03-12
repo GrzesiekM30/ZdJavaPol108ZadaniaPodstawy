0cf514bd7747c8e6597a39e8eca94213c87486b6
Grzesiek <grzesiek138@onet.eu>
2022-03-12 13:11:17 +0100

    day_task7 solution

---
+++ src/pl/sdacademy/java/basic/exercises/day3/Task7/Main.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.java.basic.exercises.day3.Task7;
+
+public class Main {
+    public static void main(String[] args) {
+         Cat [] cats = new Cat[3];
+        cats[0] = new Cat("Mruczke");
+        cats[1] = new Cat("Filemon");
+        cats[2] = new Cat("Bonifacy");
+        int i = 2;
+        for(Cat element: cats){
+            element.print();
+            element.makeSound();
+            element.eatMice(i++ * i);
+            System.out.println();
+        }
+    }
+}
